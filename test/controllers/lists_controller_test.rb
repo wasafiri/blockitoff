968fd44e6f8b75d08b0daae702a8ad2560eab1fc
David Bahar <dbahar@gwmail.gwu.edu>
2014-06-03 18:34:10 -0400

    Using Slim for lists and tasks now

---
+++ test/controllers/lists_controller_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class ListsControllerTest < ActionController::TestCase
+  def test_create
+    get :create
+    assert_response :success
+  end
+
+  def test_show
+    get :show
+    assert_response :success
+  end
+
+  def test_destroy
+    get :destroy
+    assert_response :success
+  end
+
+end
---
+++ test/controllers/tasks_controller_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+
+class TasksControllerTest < ActionController::TestCase
+  def test_new
+    get :new
+    assert_response :success
+  end
+
+  def test_create
+    get :create
+    assert_response :success
+  end
+
+  def test_show
+    get :show
+    assert_response :success
+  end
+
+  def test_destroy
+    get :destroy
+    assert_response :success
+  end
+
+end
