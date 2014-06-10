88a90d89d943792ed8481d7534dbf2d0e47fdc79
David Bahar <dbahar@gwmail.gwu.edu>
2014-06-10 10:28:23 -0400

    Installed devise gem, added users functionality

--- Gemfile
+++ Gemfile
@@ -3,10 +3,13 @@ ruby '2.1.2'
 gem 'rails', '4.1.1'
 
 gem 'devise', '3.2.4'
+gem 'figaro'
 gem 'minitest-rails', '2.0.1'
 gem 'slim-rails', '2.1.4'
 gem 'timecop', '0.7.1', group: [:development, :test]
 gem 'bootstrap-sass', '3.1.1.1'
+gem 'rails_12factor', group: [:production]
+gem 'letter_opener', group: [:development]
 
 # Use postgresql as the database for Active Record
 gem 'pg', '0.17.1'
--- test/controllers/task_completion_controller_test.rb
+++ test/controllers/task_completion_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
-class TaskCompletionControllerTest < ActionController::TestCase
-  def test_sanity
-    flunk "Need real tests"
+describe Task do
+  describe "task completion" do
+    subject { Task.new(description: "do some stuff", created_at: Time.now) }
+
+    it "must show the task as being marked complete" do
+      expect (subject.completed_at).to be_nil
+      visit_task task
+        click_link "Mark Complete"
+    end
+    task.reload
+    expect(subject.completed_at.to_not be_nil)
   end
 end
